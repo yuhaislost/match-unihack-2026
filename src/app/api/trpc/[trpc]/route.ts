3f7cd62b0c7a785fc3fe4c8c1518040c4f7068b7
Yuhang Li <68719011+yuhaislost@users.noreply.github.com>
2026-03-14 01:26:54 +1100

    fix: update import paths for createTRPCContext and appRouter inside route.ts

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,7 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { createTRPCContext } from "./trpc/init";
-import { appRouter } from "./trpc/routers/_app";
+import { createTRPCContext } from "@/trpc/init";
+import { appRouter } from "@/trpc/routers/_app";
+
 const handler = (req: Request) =>
     fetchRequestHandler({
         endpoint: "/api/trpc",
